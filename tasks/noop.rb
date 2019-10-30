7897dd888df254e671307cb5a487a573effe607a
Matthew Mallard <matt@Q-Technologies.com.au>
2019-10-30 12:18:28 +1100

    Updated the ruby scripts to fully comply with rubocop

--- tasks/noop.rb
+++ tasks/noop.rb
@@ -11,19 +11,21 @@ require 'facter'
 require 'puppet'
 require 'puppet/util/inifile'
 
-puppet_conf = '/etc/puppetlabs/puppet/puppet.conf'
-$noop_save = '/etc/puppetlabs/puppet/.noop.conf'
 if Facter.value(:kernel) == 'windows'
-  puppet_conf = 'C:\ProgramData\PuppetLabs\puppet\etc\puppet.conf'
-  $noop_save = 'C:\ProgramData\PuppetLabs\puppet\etc\.noop.conf'
+  PUPPET_CONF = 'C:\ProgramData\PuppetLabs\puppet\etc\puppet.conf'.freeze
+  NOOP_SAVE = 'C:\ProgramData\PuppetLabs\puppet\etc\.noop.conf'.freeze
+else
+  PUPPET_CONF = '/etc/puppetlabs/puppet/puppet.conf'.freeze
+  NOOP_SAVE = '/etc/puppetlabs/puppet/.noop.conf'.freeze
 end
-$file = Puppet::Util::IniConfig::File.new
-$file.read(puppet_conf)
+
+FILE = Puppet::Util::IniConfig::File.new
+FILE.read(PUPPET_CONF)
 
 def restore_previous_setting
   puts 'Seeing if there was a previous noop setting'
-  if File.exist?($noop_save)
-    noop = File.read($noop_save).chomp
+  if File.exist?(NOOP_SAVE)
+    noop = File.read(NOOP_SAVE).chomp
   end
   if noop =~ %r{^(0|1|true|false)$}i
     puts "It was found to be '#{noop}', restoring it to the puppet.conf file"
@@ -36,18 +38,18 @@ end
 
 def save_noop_puppet_conf(noop)
   puts "Setting the noop setting to '#{noop}'"
-  $file['agent']['noop'] = noop
-  $file.store
+  FILE['agent']['noop'] = noop
+  FILE.store
 end
 
 def save_previous_setting
   puts 'Seeing if there is a current noop setting'
-  noop = $file['agent']['noop']
+  noop = FILE['agent']['noop']
   if noop.nil?
     puts 'There is no current setting'
   else
     puts "The current setting is '#{noop}', saving it so the new setting can be reverted later"
-    open($noop_save, 'w') { |f| f.puts noop }
+    open(NOOP_SAVE, 'w') { |f| f.puts noop }
   end
 end
 
@@ -61,7 +63,7 @@ when 'on'
 when 'revert'
   restore_previous_setting
 when 'show'
-  puts $file['agent']['noop'] || 'unset'
+  puts FILE['agent']['noop'] || 'unset'
 else
   STDERR.puts 'Unknown action or it was not specified'
   exit 1
